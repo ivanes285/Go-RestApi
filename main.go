1367d93dcc3b96819ddf08727ee87c9818ffbabe
ivanes285 <ivanes285@gmail.com>
2023-01-26 21:13:51 -0500

    Add all methos http for the API rest

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"fmt"                    // fmt implementa funciones o metdos de formateado de texto
 	"github.com/gorilla/mux" // gorilla/mux is a powerful URL router and dispatcher for golang
-	"log"
-	"net/http"    // net/http package provides HTTP client and server implementations
+	"io/ioutil"
+	"log"      // log writes to standard error and prints the date and time of each logged message
+	"net/http" // net/http package provides HTTP client and server implementations
+	"strconv"  // strconv permite realizar conversiones de tipos de datos
 )
 
 type Task struct {
@@ -14,7 +17,7 @@ type Task struct {
 }
 type AllTasks []Task
 
-var taks = AllTasks{
+var tasks = AllTasks{
 	{
 		Id:      1,
 		Name:    "Task One",
@@ -38,20 +41,105 @@ var taks = AllTasks{
 	},
 }
 
-func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: homePage")
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func getTaskById(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)["id"]     // mux.Vars retorna un mapa con los parametros de la ruta
+	Id, err := strconv.Atoi(vars) // strconv.Atoi convierte un string a un entero
+	if err != nil {
+		fmt.Fprintf(w, "Id Inválido")
+		return
+	}
+
+	for _, task := range tasks {
+		if task.Id == Id {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(200)
+			json.NewEncoder(w).Encode(task)
+		}
+	}
 
 }
-func taskLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: taskPage")
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var newTask Task
+	reqBody, err := ioutil.ReadAll(r.Body) // ioutil permite manejar las entradas y salidas de datos de forma mas sencilla
+	if err != nil {                        // nil es un valor nulo
+		w.WriteHeader(404)
+		fmt.Fprintf(w, "Inserta una tarea valida")
+		return // return finaliza la ejecucion de la funcion
+	}
+	json.Unmarshal(reqBody, &newTask)                  // json.Unmarshal decodifica el json y lo guarda en la variable newTask
+	newTask.Id = len(tasks) + 1                        // le asignamos un id a la nueva tarea
+	tasks = append(tasks, newTask)                     // append agrega un nuevo elemento al final de la lista
+	w.Header().Set("Content-Type", "application/json") // Set establece el valor de la cabecera
+	w.WriteHeader(200)                                 // http.StatusCreated es el codigo de estado de la respuesta
+	json.NewEncoder(w).Encode(newTask)                 // json.NewEncoder devolvera la lista de tareas
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	taskId := mux.Vars(r)["id"] // mux.Vars retorna un mapa con los parametros de la ruta
+	var newTasks AllTasks
+	Id, err := strconv.Atoi(taskId) // strconv.Atoi convierte un string a un entero
+	if err != nil {
+		w.WriteHeader(404)
+		fmt.Fprintf(w, "Id Inválido")
+		return
+	}
+	for i, task := range tasks {
+		if task.Id != Id {
+			newTasks = append(newTasks, task) // append agrega un nuevo elemento al final de la lista
+			fmt.Println(i)                    // i es el indice del arreglo de tareas en este caso lo usamos para imprimirlo no es necesario
+		}
+	}
+	tasks = newTasks
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "Tarea con Id %v eliminada correctamente", Id) // enviara un mensaje de confirmacion de la eliminacion de la tarea
+}
+ func updateTask(w http.ResponseWriter, r *http.Request) {
+	taskId := mux.Vars(r)["id"]         // mux.Vars retorna un mapa con los parametros de la ruta
+	Id, err := strconv.Atoi(taskId)
+	if err != nil {
+		w.WriteHeader(404)
+		fmt.Fprintf(w, "Id Inválido")
+		return
+	}
+   var updatedTask Task
+	reqBody, err := ioutil.ReadAll(r.Body) 
+	if err != nil {
+	  fmt.Fprintf(w, "Inserta datos válidos")
+	}
+	json.Unmarshal(reqBody, &updatedTask)
+	for i, task := range tasks {
+		if task.Id == Id {
+			tasks = append(tasks[:i], tasks[i+1:]...) //tasks[:i] toma todos los elementos menos el elemento en la posicion i y tasks[i+1:] toma todos los elementos a partir de la posicion i+1 hasta el final del arreglo y ... es para que se pueda concatenar
+			updatedTask.Id = Id
+			tasks = append(tasks, updatedTask)      // append agrega un nuevo elemento al final de la lista en este caso la tarea actualizada
+			fmt.Fprintf(w, "La tarea con Id %v ha sido actualizada correctamente", Id)
+		}
+	}
+	 
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Bienvenido a la API REST de tareas realizada en GO")
 
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homeLink)      //nueva ruta para homeLink
-	router.HandleFunc("/tasks", taskLink) //nueva ruta para taskLink
-	//creamos un servidor http
-	log.Fatal(http.ListenAndServe(":4000", router))
+	router.HandleFunc("/", home)
+	router.HandleFunc("/tasks", getTasks).Methods("GET")
+	router.HandleFunc("/tasks/{id}", getTaskById).Methods("GET")
+	router.HandleFunc("/tasks", createTask).Methods("POST")
+	router.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
+	router.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":3000", router))
 
 }
